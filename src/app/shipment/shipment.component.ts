3757acf6c33793dd2226f766edd11279193894ed
Nathan Getachew <nathygech111@gmail.com>
2019-09-14 19:49:47 +0300

    shipment component to call shipment data

--- src/app/shipment/shipment.component.ts
+++ src/app/shipment/shipment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-shipment',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shipment.component.css']
 })
 export class ShipmentComponent implements OnInit {
+  shipData: object;
+  disp: boolean;
+  carrier: string;
+  booking_ref: string;
+  bl_ref: string;
+  sea_mvmnt: object[];
+  now: number;
+  month: string;
+  constructor(private dataService: DataService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  callShipmentData() {
+    this.now = Date.now();
+    if (!this.shipData) {
+      this.dataService.getShipmentTimeLine().subscribe(data => {
+        if (data) {
+          this.shipData = data;
+          this.carrier = data['sea_shipments'][0]['carrier']['name'];
+          this.booking_ref = data['sea_shipments'][0]['booking_reference'];
+          this.bl_ref = data['sea_shipments'][0]['bill_of_lading_reference'];
+          this.sea_mvmnt = data['sea_shipments'][0]['sea_movements'];
+          this.disp = true;
+        }
+      });
+    }
   }
-
 }
