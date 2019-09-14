3c89d8d6a53dd6d12aae70fcb3b05a0c35bbe434
Nathan Getachew <nathygech111@gmail.com>
2019-09-14 13:24:00 +0300

    add http client in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShipmentComponent } from './shipment/shipment.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ShipmentComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ShipmentComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
